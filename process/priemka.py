224a532789be0f45900d82e462eee7b811a31f3f
miraiy-outh <shokolatemeur@mail.ru>
2021-12-09 14:21:34 +0300

    1

---
+++ process/priemka.py
@@ -0,0 +1,50 @@
+# продукты для теста - для 1 партии используется 1000 ед. сырья; задержка - 60 сек.
+def priemka_dough():
+    global flour
+    global sugar
+    global milk
+    global water
+    global egg
+    global yeast
+
+    flour += 2000
+    sugar += 2000
+    milk += 2000
+    water += 2000
+    egg += 2000
+    yeast += 2000
+
+
+# фруктово-ягодная начинка - для 1 партии используется 50 ед. сырья
+# скоропортящаяся, поэтому поставляется маленькими партиями и чаще (10 сек), чем другое сырье
+def priemka_fill_fr_b():
+    global apple
+    global raspberry
+    global strawberry
+    global lemon
+    global cherry
+    global peach
+    global berries
+    apple = 100
+    raspberry = 100
+    strawberry = 100
+    lemon = 100
+    cherry = 100
+    peach = 100
+    berries = 100
+
+
+# долгохранящаяся начинка - для 1 партии используется 50 ед. сырья; поставляется часто (30 сек)
+def priemka_fill_long():
+    global chocolate
+    global creme
+    global caramel
+    global poppy
+    global cinnamon
+    global vanilla
+    chocolate = 200
+    creme = 200
+    caramel = 200
+    poppy = 200
+    cinnamon = 200
+    vanilla = 200
